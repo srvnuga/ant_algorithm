8bcb01c97594b42a6ce05f7ef090cabe21a39298
gebond <gebond77@gmail.com>
2017-01-18 18:07:25 +0400

    - created package - moving files to entities package - created main_program - entry point

---
+++ main_program.py
@@ -0,0 +1,9 @@
+from entities.ant import Ant
+from entities.cell import Cell
+
+# testing creating of Ant object
+ant = Ant()
+
+# testing creating of Cell object
+cell = Cell()
+cell.set_feromon(123)
