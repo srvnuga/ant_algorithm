925e48919acd9b064faebd925504bd396846ce99
gebond <gebond77@gmail.com>
2017-01-18 18:32:05 +0400

    separated method for Ant moving

--- entities/ant.py
+++ entities/ant.py
@@ -5,10 +5,12 @@ class Ant:
     def __init__(self):
         pass
 
-    def get_selected_cell_id(self, available_cells):  # available_cells = это список
+    def get_selected_cell_id(self, available_cells):  # available_cells - list of cells
+        # todo add logic for finding cell with maximum feromon
         selected_id = 0
-        self.__old_cell = self.__current_cell
-        self.__current_cell = selected_id
+        self.move(selected_id)
         return selected_id
 
-
+    def move(self, new_cell):
+        self.__old_cell = self.__current_cell
+        self.__current_cell = new_cell
