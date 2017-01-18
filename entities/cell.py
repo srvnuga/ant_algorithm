28f8d8e7c52b5303b4a5cbaeb64c03580cece6fe
srvnuga <gaya4ka.alexsanyan@mail.ru>
2017-01-18 20:44:50 +0400

    added methods in Cell

--- entities/cell.py
+++ entities/cell.py
@@ -1,11 +1,14 @@
 class Cell:
     __state = None  # Cell can be in 4 states currently: 0 - normal, 1 - start, 2 - finish, 3 - permitted
     __feromone = None
-
-    # todo add counter
+    __counter = None
 
     def __init__(self, feromone, state=0):
-        # todo add validations of input(waiting only type Int, Double)
+        if not isinstance(feromone, float):
+            raise TypeError("input feromone is not %s type" % (float))
+        if not isinstance(state, int) and (-1 < state < 4):
+            raise TypeError("input state is not %s type must be 0,1,2,3" % (int))
+        self.__counter = 0
         self.__feromone = feromone
         self.__state = state
 
@@ -15,7 +18,11 @@ class Cell:
     def is_start(self):
         return self.__state == 1
 
-    # todo add more checkers
+    def is_finish(self):
+        return self.__state == 2
+
+    def is_permitted(self):
+        return self.__state == 3
 
     def set_feromon(self, feromone):
         self.__feromone = feromone
@@ -23,4 +30,9 @@ class Cell:
     def get_feromon(self):
         return self.__feromone
 
-    # todo add method for calculating and getting value of counter
+    def increase_counter(self):
+         self.__counter += 1
+
+
+    def get_counter(self):
+        return self.__counter
