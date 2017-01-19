77812b2cb620361f8a8fe318d844212c51e8690c
srvnuga <gaya4ka.alexsanyan@mail.ru>
2017-01-19 12:37:43 +0400

    Ant for gebond

--- entities/ant.py
+++ entities/ant.py
@@ -1,3 +1,5 @@
+import random
+
 class Ant:
     __old_cell = None
     __current_cell = None
@@ -7,10 +9,26 @@ class Ant:
 
     def get_selected_cell_id(self, available_cells):  # available_cells - list of cells
         # todo add logic for finding cell with maximum feromon
-        selected_id = 0
+        if not isinstance(available_cells, list):
+            raise TypeError("input available_cells is not %s type" % (list))
+        max_feromon = 0.0
+        for cell in available_cells:
+            if cell.get_feromon() > max_feromon:
+                max_feromon = cell.get_feromon()
+        max_cells = []
+        for cell in available_cells:
+            if cell.get_feromon() is max_feromon:
+                max_cells.append(cell)
+        selected_id = None
+        if len(max_cells) == 1:
+            selected_id = max_cells[0].get_id()
+        else:
+            random_cell = random.randrange(0, len(max_cells), 1)
+            selected_id = max_cells[random_cell].get_id()
         self.move(selected_id)
         return selected_id
 
     def move(self, new_cell):
         self.__old_cell = self.__current_cell
         self.__current_cell = new_cell
+
--- main_program.py
+++ main_program.py
@@ -2,8 +2,17 @@ from entities.ant import Ant
 from entities.cell import Cell
 
 # testing creating of Ant object
-ant = Ant()
+ant1111 = Ant()
 
 # testing creating of Cell object
-cell = Cell(k)
-cell.set_feromon(123)
+#cell = Cell()
+#cell.set_feromon(123)
+
+a = Cell(1, 9999.0)
+b = Cell(2, 888.0)
+c = Cell(3, 9999.0)
+
+av_cells = [a, b, c]
+
+selected = ant1111.get_selected_cell_id(av_cells)
+print(selected)
