480a3a3d514207367ebc556382b7a779db37c41c
gebond <gebond77@gmail.com>
2017-01-19 12:53:38 +0400

    hotfix todo items

--- entities/controller.py
+++ entities/controller.py
@@ -8,21 +8,20 @@ class Controller:
 
     def __init__(self, sizeOfBoard):
         # todo add init ant
-        # todo init number of iteration
+        # todo init number of iteration with zero
         self.__board = Board(sizeOfBoard)
 
     def start(self, max_number_of_iteration):
         # todo add logic for start process:
         # ant must be on start cell
-        # controller must gte from board available cells and take them to ant
-        # then controller get the answer from ant(id of cell which ant select)
-        # and controller send this selected id to the board
         # it's required to use method process() until number of iteration < max_number_of_iteration
-
         pass
 
     def process(self):
-        # todo logic for ONE iteration must be added here
+        # todo logic for ONE iteration must be added here:
+        # controller must get from board available cells and take them to ant
+        # then controller get the answer from ant(id of cell which was selected by ant)
+        # and controller send this selected id to the board
         # in the end of process number of iteration must be +1
         pass
 
